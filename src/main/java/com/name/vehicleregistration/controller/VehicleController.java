ce157b2479e1d0479782b9b544e94b616219ef40
Isaias Malvar <38163984+IsaiasMalvar@users.noreply.github.com>
2024-10-19 15:16:51 +0100

    Update VehicleController.java

--- src/main/java/com/name/vehicleregistration/controller/VehicleController.java
+++ src/main/java/com/name/vehicleregistration/controller/VehicleController.java
@@ -9,11 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
-
 import lombok.extern.slf4j.Slf4j;
-
 import java.util.NoSuchElementException;
 
 @RestController
@@ -33,7 +29,7 @@ public class VehicleController {
         try{
             return ResponseEntity.ok(vehicleService.getVehicleById(id));
         } catch (NoSuchElementException e) {
-            log.info(e.getMessage());
+           
             return  ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         } catch (Exception e) {
             throw new RuntimeException(e);
